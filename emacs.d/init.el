ee6a7850838501cf70304649149b87baa6c2cfa9
Michael Mulich <michael.mulich@gmail.com>
2015-08-29 10:07:07 -0700

    Configure emacs to use spaces instead of tabs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,6 +2,9 @@
 ;; Disable backup files
 (setq make-backup-files nil)
 
+;; Make all tab input spaces instead of the tab char.
+(setq-default indent-tabs-mode nil)
+
 ;; Packages please!
 (require 'package)
 (add-to-list 'package-archives
