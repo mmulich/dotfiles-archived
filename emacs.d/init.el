6baab597fbb38258d69b76f3389cffda632d048b
Michael Mulich <michael.mulich@gmail.com>
2015-08-29 10:10:09 -0700

    Enable emac's column-number-mode for all buffers.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,6 +5,9 @@
 ;; Make all tab input spaces instead of the tab char.
 (setq-default indent-tabs-mode nil)
 
+;; Enable column-number-mode everywhere
+(setq column-number-mode t)
+
 ;; Packages please!
 (require 'package)
 (add-to-list 'package-archives
