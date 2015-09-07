26a84aaac1a8034e2bf3c95f35d9eb4977ec3c46
Michael Mulich <michael.mulich@gmail.com>
2015-09-06 23:23:03 -0700

    Add xmobar to xmonad configuration

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,9 +6,14 @@ import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
 main = do
+  xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
          { manageHook = manageDocks <+> manageHook defaultConfig
          , layoutHook = avoidStruts  $  layoutHook defaultConfig
+         , logHook = dynamicLogWithPP xmobarPP
+                   { ppOutput = hPutStrLn xmproc
+                   , ppTitle = xmobarColor "grey" "" . shorten 50
+                   }
          , modMask = mod4Mask  -- Rebinds Mod to Command (cmd) key
          } `additionalKeysP`
          [ ("M-z", spawn "xscreensaver-command -lock")
